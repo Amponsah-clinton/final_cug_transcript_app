998aa1c7da327ea7954f00c9a8bf34c6b0c0e486
Amponsah-clinton <amponsahc304@gmail.com>
2025-10-24 03:27:26 -1200

    Updated transcript form and fixed template bugs

--- transcript_project/settings.py
+++ transcript_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
      'app',
       'widget_tweaks',
+          'whitenoise',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+      'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'transcript_project.urls'
